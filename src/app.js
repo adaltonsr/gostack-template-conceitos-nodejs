64713a4b492f08d12070f19802ceaa6a75ea60fa
Adalton Syllas <adalton@nukz.com.br>
2020-04-15 02:29:49 -0300

    update routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,79 @@ app.use(cors());
 
 const repositories = [];
 
+function checkIdExist(id){
+	const  repositoryIndex	= repositories.findIndex(repository => repository.id == id) 
+	return repositoryIndex === -1 ? -1 : repositoryIndex 
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+	return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = {
+		id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+	const { id } = request.params
+	repositoryIndex = checkIdExist(id)
+
+	if (repositoryIndex === -1)
+		return response.status(400).json('Repository does not exist') 
+	
+	const { title, url, techs } = request.body
+	const repository = {
+		id,
+		title,
+		url,
+		techs,
+		likes: repositories[repositoryIndex].likes,
+	}
+
+	repositories[repositoryIndex] = repository
+	return response.json(repository)	
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+	const { id } = request.params
+	repositoryIndex = checkIdExist(id)
+	
+	if (repositoryIndex === -1)
+		return response.status(400).json('Repository does not exist') 
+	
+	repositories.splice(repositoryIndex, 1)
+
+	return response.status(204).json()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+	const { id } = request.params
+	repositoryIndex = checkIdExist(id)
+	if (repositoryIndex === -1)
+		return response.status(400).json('Repository does not exist') 
+	
+	like = repositories[repositoryIndex].likes+=1
+
+	const repository = {
+		id,
+		title: repositories[repositoryIndex].title,
+		url:repositories[repositoryIndex].url,
+		techs: repositories[repositoryIndex].techs,
+		likes: like
+	}
+	
+	repositories[repositoryIndex] = repository	
+
+	return response.json(repository)
 });
 
 module.exports = app;
